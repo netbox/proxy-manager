a51b3a5c9a867bea4393e1f383b005cea81f2b5f
Dmitriy Protasov <dmitriy.protasov@gmail.com>
2012-09-07 00:13:59 +0400

    skeleton

---
+++ src/NetBox/ProxyManager/ProxyManager.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the ProxyManager package.
+ * Date: 06.09.12
+ * Time: 23:55
+ *
+ * (c) Dmitriy Protasov <dmitriy.protasov@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ProxyManager;
+
+class ProxyManager
+{
+
+}
